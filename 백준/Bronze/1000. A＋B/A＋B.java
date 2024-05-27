e5792fee4191bf1edfe625eac5648c799df0f789
Choi ho jin <127203896+gentle-tiger@users.noreply.github.com>
2024-05-28 00:26:50 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        
+    
+    Scanner kb = new Scanner(System.in);
+    
+    int n = kb.nextInt();
+    int m = kb.nextInt();
+    
+    System.out.println(n+m);
+    
+
+    }
+}
