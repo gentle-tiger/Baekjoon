dfe4ca8656ead0b6a72606b9489b33621214cdc9
Choi ho jin <127203896+gentle-tiger@users.noreply.github.com>
2024-06-01 10:07:07 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,13 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
         public static void main(String[] args) throws IOException {
 
-            Scanner sc = new Scanner(System.in);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String[] input = br.readLine().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
 
-            int n = sc.nextInt(); // 24
-            int m = sc.nextInt(); // 18
+//            int n = sc.nextInt(); // 24
+//            int m = sc.nextInt(); // 18
+
             int min = 1; // 최대 공약수 6
             int max = 1; // 최소 공배수 72
 
@@ -25,3 +30,14 @@ public class Main {
             System.out.println(max);
         }
 }
+
+// 이전에 해결하려 했던 최소공배수
+//            for(int i = 1; i < 20; i++) {
+//                if(n*i == m){
+//                    max = n*i;
+//                    System.out.println(min);
+//                    System.out.println(max);
+//
+//                }
+//            }
+
