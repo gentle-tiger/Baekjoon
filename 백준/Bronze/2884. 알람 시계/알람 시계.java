4f8865cb31fd266e41838f31dcd56a8cf23d36d5
Choi ho jin <127203896+gentle-tiger@users.noreply.github.com>
2024-06-25 12:37:18 +0900

    [Bronze III] Title: 알람 시계, Time: 136 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -12,37 +12,18 @@ public class Main {
         int H = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-// M > 45일 때, H가
-// H != 0 이고, M이 높을 때 or 낮을 때
 
-
-//      M부터 확인해야 함.
-        if(M >= 45){
-            int newM = M - 45;
-            System.out.print(H+ " ");
-            System.out.print(newM);
-//      M이 45분보다 적고 H가 0일 때
-        } else{
-            // 0시라면
-            if(H == 0){
-                int newH = H +23;
-                int newM = M+60-45;
-                if(newM == 60){
-                    newM--;
-
-                }
-                System.out.print(newH+ " ");
-                System.out.print(newM);
-            }else{
-            // 0시가 아니라면
-                int newH = H-1;
-                int newM = M+60-45;
-                if(newM == 60){
-                    newM--;
-                }
-                System.out.print(newH+ " ");
-                System.out.print(newM);
+        if(M < 45){
+            H--;
+            M = (M+60)-45;
+            if(H < 0){
+                H = 23;
             }
+        }else{
+            M = M - 45;
         }
+        System.out.print(H);
+        System.out.print(" ");
+        System.out.print(M);
     }
 }
