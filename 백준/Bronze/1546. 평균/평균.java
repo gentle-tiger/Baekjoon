d8d3e8e2f13497bf23b0d2e08b13ba2eb151e6df
Choi ho jin <127203896+gentle-tiger@users.noreply.github.com>
2024-07-07 03:34:43 +0900

    [Bronze I] Title: 평균, Time: 116 ms, Memory: 16428 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(String.valueOf(br.readLine()));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        float[] tokens = new float[N];
+        for(int i=0; i<N; i++ ){
+            tokens[i] = Float.parseFloat(st.nextToken());
+        }
+
+
+        Arrays.sort(tokens);
+
+        float max = tokens[tokens.length-1];
+        float sum = 0;
+
+        for (float  token : tokens) {
+            sum += token;
+        }
+        float result = (sum / max * 100) / N;
+
+        System.out.println(result);
+    }
+}
