9982f2ddd1ef71134ec74b92d7bb0761e2705a7e
Choi hojin <127203896+gentle-tiger@users.noreply.github.com>
2024-10-21 10:50:02 +0900

    요세푸스 문제.java

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -11,18 +11,22 @@ import java.util.StringTokenizer;
 
 /*
  * ** 문제 파악
- * 1.
+ * 1. 1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있으며, 순서도래 K번째 사람을 제거한다.
+ * 2. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나가며, N명의 사람이 모두 제거될 때까지 계속된다.
  * ** 유의할 점
- * 1.
+ * 1. 제거된 사람들을 제외한 나머지 중에서 다시 이 과정이 이루어진다는 점.
  * ** 접근 방법
- * 1.
+ * 1. List의 크기가 동적으로 변경되는 특성을 활용하여, List에 N명을 담고, 순서대로 제거하는 과정을 반복함.
  * ** 어려웠던 점
- * 1.
+ * 1. 처음에는 제거된 사람들을 제외하는지 몰라서 문제 푸는데에 어려움을 겪음.
  * 2.
  *
  * ** 개선할 점
- * 1.
- *
+ * 1. 문제는 ArrayList를 만들어 풀었지만, 동작 로직을 보면 큐와 동일한 로직으로 진행되는 것을 알 수 있다.
+ * 2. 큐에 대해 공부하고 이를 적용해봐야 할 것 같다.
+ *    - ArrayList로 문제를 풀었을 때, 시간이 3340ms가 걸렸다면, Queue는 768ms 만에 해결했다.
+ *    - ArrayList는 메모리  상에서 연속된 공간에 할당되며, 그 연속성을 유지하기 위해서는 나머지 요소들이 새로운 배열의 처음부터 다시 시작해야 한다.
+ *    - Queue는 메모리 상에서 비연속적으로 저장되지만 각 노도는 다음 노드르르 가리키는 포인터를 통해 서로 연결되어 있기 때문에, 중간에 요소가 제거되어도 노드 간의 참조만 변경된다.
  */
 public class Main {
 
@@ -38,43 +42,44 @@ public class Main {
     int s = Integer.parseInt(st.nextToken()); // 7
     int n = Integer.parseInt(st.nextToken()); // 3
 
-    List<Integer> q = new ArrayList<>();
+//    List<Integer> peopleList = new ArrayList<>();
+    Queue<Integer> q = new LinkedList<>();
 
-    for (int i = 1; i <= s; i++) {
+    // 1 2 3 4 5 6 7
+    for(int i = 1; i <= s; i++) {
+//      peopleList.add(i);
       q.add(i);
     }
 
-    int idx = 0; // 인덱스값
-    int reset = 0; // 3번마다
-    int len = 0;
+    // 3번의 카운트를 할 변수
+    int count = 0;
 
-    bw.write('<');
-
-    while (!q.isEmpty()) {
-
-      reset++;
-
-      // 3번을 순회했다면
-      if (reset == n) {
-        if (len >= s - 1) {
-          bw.write(Integer.toString(q.remove(idx)));
-          break;
+     bw.write("<");
+//    while(!peopleList.isEmpty()){
+    while(!q.isEmpty()){
+//      int curr = peopleList.remove(0);
+      int currQ = q.remove();
+      count++;
+      // 3번을 다 돌았다면
+      if(count == n){
+        //  peopleList에 하나의 값만 남았을 경우
+//        if(peopleList.isEmpty()) {
+        if (q.isEmpty()){
+//          bw.write(Integer.toString(curr));
+          bw.write(Integer.toString(currQ));
+        }else{
+//          bw.write(curr + ", ");
+          bw.write(currQ + ", ");
         }
-        // 배열의 length에 q의 값을 값을 넣음.
-        bw.write(q.remove(idx) + ", ");
-        // 값을 넣었다면 reset의 값을 초기화 해주고 배열의 길이를 올려줌.
-        reset = 0;
-        len++;
-      } else {
-        idx++;
-      }
-
-      if (idx >= q.size()) {
-        idx = 0;
+        count = 0;
+      }else{
+        // 3번을 다 돌지 않았다면 그 값을 맨 뒤에 추가.
+//        peopleList.add(curr);
+        q.add(currQ);
       }
     }
+    bw.write(">");
 
-    bw.write('>');
 
     bw.flush();
 
