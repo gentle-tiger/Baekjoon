0f6433f59015e098ac79f41c0f4b49d10c5f63f9
Choi Hojin <jkhh9725@gmail.com>
2025-01-10 02:42:14 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        if (n >= 2) {
+            dp[2] = 2;
+        }
+
+
+        // 점화식 계산
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        
+        System.out.println(dp[n] % 10007);
+    }
+}
