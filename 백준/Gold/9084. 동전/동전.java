67af5e37801ad2fc85d070f2ea4f915676fd1fda
Choi Hojin <jkhh9725@gmail.com>
2025-02-24 11:39:58 +0900

    [Gold V] Title: 동전, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -4,35 +4,38 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int N;
-    static int[] coins;
-    static int M;
-    static int result;
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static int coins[];
+    static int answerCoin;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
         for(int t = 0; t < T; t++){
 
-            N = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine());
+            coins = new int[n];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            coins = new int[N];
-            
-            for(int n = 0; n < N; n++){
-                coins[n] = Integer.parseInt(st.nextToken());
+            for(int c = 0; c < n; c++){
+                int coin = Integer.parseInt(st.nextToken());
+                coins[c] = coin;
             }
-            M = Integer.parseInt(br.readLine());
-            
-            int[] dp = new int[M + 1];
-            dp[0] = 1; 
 
-            for(int coin : coins){
-                for(int j = coin; j <= M; j++){
-                    dp[j] += dp[j - coin];
-                }
+            answerCoin = Integer.parseInt(br.readLine());
+
+            int[] dp = new int[answerCoin + 1];
+
+            dp[0] = 1; // 왜 1인가? -> 아무 동전도 사용하지 않는 방법. 
+
+            
+            for (int coin : coins) {
+                for (int i = coin; i <= answerCoin; i++) {
+                    dp[i] += dp[i-coin];
+                }                
             }
-            System.out.println(dp[M]);
+            System.out.println(dp[answerCoin]);
         }
+        
     }
 }
