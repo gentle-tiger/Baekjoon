09b2c6c29eeb4d015b28a946aa501dd3a24d2501
Choi Hojin <jkhh9725@gmail.com>
2025-04-18 16:41:39 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,36 +1,30 @@
-import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String answer = br.readLine();
-        
-        StringTokenizer minusTk = new StringTokenizer(answer, "-");
+        String input = br.readLine();
 
-        int result = 0; 
-        boolean isFirst = true;
+        String[] minusSplit = input.split("-");
 
-        while(minusTk.hasMoreTokens()){
-            String token = minusTk.nextToken();
+        int result = 0;
 
-            StringTokenizer plusTt = new StringTokenizer(token, "+");
+        for (int i = 0; i < minusSplit.length; i++) {
+            String[] plusSplit = minusSplit[i].split("\\+"); // +는 정규표현식 특수문자니까 \\+로 써야 함
+            int sum = 0;
 
-            int sum = 0; 
-            while(plusTt.hasMoreTokens()){
-                sum += Integer.parseInt(plusTt.nextToken());
+            for (String num : plusSplit) {
+                sum += Integer.parseInt(num);
             }
 
-            if(isFirst){
+            if (i == 0) {
                 result += sum;
-                isFirst = false;
-            }else{
+            } else {
                 result -= sum;
             }
         }
-        System.out.print(result);
+
+        System.out.println(result);
     }
 }
