0b5e3b35aac3ee20062788f6f197ec80bf33d80f
Choi Hojin <jkhh9725@gmail.com>
2025-05-09 22:47:02 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,30 +1,40 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
 
-        String[] minusSplit = input.split("-");
+        String answer = br.readLine();
+        StringTokenizer minusToken = new StringTokenizer(answer, "-");
 
-        int result = 0;
+        int result = 0; 
+        boolean isFirst = true; 
 
-        for (int i = 0; i < minusSplit.length; i++) {
-            String[] plusSplit = minusSplit[i].split("\\+"); // +는 정규표현식 특수문자니까 \\+로 써야 함
+        while(minusToken.hasMoreTokens()){
+            String token = minusToken.nextToken();
+            // System.out.print("현재 Token값 : " + token);
+            StringTokenizer plusToken = new StringTokenizer(token, "+");
+
             int sum = 0;
-
-            for (String num : plusSplit) {
-                sum += Integer.parseInt(num);
+            // +로 묶여있는 토큰의 수만큼 sum
+            while(plusToken.hasMoreTokens()){
+                sum += Integer.parseInt(plusToken.nextToken());
             }
 
-            if (i == 0) {
-                result += sum;
-            } else {
-                result -= sum;
+            // System.out.println(" |  sum = " + sum);
+
+            if(isFirst){
+                result += sum; 
+                isFirst = false;
+            }else{
+                result -= sum; 
             }
         }
-
+        
         System.out.println(result);
     }
 }
