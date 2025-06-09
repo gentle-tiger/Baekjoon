2720fe365b8f79ad574fbe7ea8789b9ea83c31c2
Choi Hojin <jkhh9725@gmail.com>
2025-06-09 17:45:39 +0900

    [Bronze II] Title: 시험 감독, Time: 420 ms, Memory: 92236 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -1,6 +1,8 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
+// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws IOException {
 
@@ -10,23 +12,25 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] A = new int[N];
         for(int i = 0; i < N; i++){
-            A[i] = Integer.parseInt(st.nextToken());  // 여기 수정
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         int B = Integer.parseInt(st.nextToken()); // 총감독관 감시 B명 
         int C = Integer.parseInt(st.nextToken()); // 부감독관 감시 C명 
 
-        long viewer = 0;  // 주의: long 안 쓰면 오버플로우 발생
+        long viewer = 0;
 
         for(int i = 0; i < N; i++){
             int person = A[i];
-            viewer += 1; // 총감독관 1명
-            person -= B;
+            viewer += 1; // 총 시험 감독관 인원 추가
+            person -= B; // 감시자 수 줄이기
 
+            // 감시해야할 사람이 남아 있다면,
             if(person > 0){
                 viewer += (person + C - 1) / C;
             }
+            
         }
         
         System.out.println(viewer);
