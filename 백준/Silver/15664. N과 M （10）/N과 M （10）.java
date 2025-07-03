e08c9befd2a3600b78447145d8fc2f460aac06e3
Choi Hojin <jkhh9725@gmail.com>
2025-07-03 19:44:10 +0900

    [Silver II] Title: N과 M (10), Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static int N, M;
+    static int[] path,arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        path = new int[M];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0, 0);
+        
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth, int start){
+
+        if(depth == M){
+            for(int x : path){
+                sb.append(x).append(' ');    
+            }   
+            sb.append("\n");
+            return;
+        }
+
+        int last = -1; 
+        for(int i = start; i < N; i++){
+            if(visited[i] || arr[i] == last) continue; 
+            visited[i] = true;
+            path[depth] = arr[i];
+            last = arr[i];
+            dfs(depth + 1, i);
+            visited[i] = false;
+        }
+        
+    }
+}
