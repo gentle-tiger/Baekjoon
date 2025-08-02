a82f3258465af8c92291476ff484c0d49e2f33ca
Choi Hojin <jkhh9725@gmail.com>
2025-08-02 18:27:42 +0900

    [Gold V] Title: 토마토, Time: 584 ms, Memory: 120284 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -4,82 +4,86 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    static int N, M; 
+    static int[][] box;
+    static int[] DX = {-1, 1, 0, 0};
+    static int[] DY = {0, 0, -1, 1};
+    static Queue<int[]> q = new LinkedList<>();
+    
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] node = br.readLine().split(" ");
-        int M = Integer.parseInt(node[0]); // 6
-        int N = Integer.parseInt(node[1]); // 4
+        String[] input = br.readLine().split(" ");
+        M = Integer.parseInt(input[0]);
+        N = Integer.parseInt(input[1]);
 
-        
-        int[][] box = new int[N][M]; 
-        Queue<int[]> q = new ArrayDeque<>();
+        box = new int[N][M];
 
-         
-        int[][] dist = {{1,0},{-1,0},{0,1},{0,-1}};
-        boolean[][] visited = new boolean[N][M];
-        
+        boolean not_tomato = true; 
+        boolean ripe_tomato = true;
         for(int i = 0; i < N; i++){
-            String[] line = br.readLine().split(" "); 
-            
-            for(int j = 0 ; j < M; j++){
-                box[i][j] = Integer.parseInt(line[j]);
-                // System.out.print(Integer.parseInt(line[j]) + " ");
-                
+            StringTokenizer st = new StringTokenizer(br.readLine());
+           for(int j = 0; j < M; j++){
+               box[i][j] = Integer.parseInt(st.nextToken());
                 if(box[i][j] == 1){
-                    q.add(new int[]{i, j, 1}); // 토마토 위치 찾기
-                    visited[i][j] = true; 
-                    // System.out.println(" !토마토 위치 찾기 성공! ");
+                    q.offer(new int[]{i, j});
+                    ripe_tomato = false;
                 }
-            }
-            // System.out.println();
+                // 토마토가 있다면  
+                if(box[i][j] == 0) not_tomato = false; 
+           } 
+        }
+        // 익힐 토마토가 없다면 0 반환 
+        if(not_tomato){
+            System.out.println("0");
+            return;
+        } 
+
+        // 익은 토마토가 없다면, 모두 익히지 못하므로 -1 반환
+        if(ripe_tomato){
+            System.out.println("-1");
+            return;
         }
 
+        bfs();
+        
+        int result = 0; 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if(box[i][j] == 0){
+                    System.out.println(-1);
+                    return;
+                }
+                result = Math.max(result, box[i][j]);
+            }
+        }
 
-        int answer = 0; 
+        
+        System.out.println(result - 1);
+    }
+
+    private static void bfs(){
+
         while(!q.isEmpty()){
-
-            
-            int[] cur = q.poll();  // {3,5}
-            int curRow = cur[0]; // 3
-            int curCol = cur[1]; // 5 
-            int count = cur[2];
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
 
             for(int i = 0; i < 4; i++){
-                int nextRow = curRow + dist[i][0];
-                int nextCol = curCol + dist[i][1];
-                if(0 <= nextRow && nextRow < N && 
-                   0 <= nextCol && nextCol < M &&
-                   box[nextRow][nextCol] == 0 && !visited[nextRow][nextCol])
+                int nx = x + DX[i];
+                int ny = y + DY[i];
+                if(0 <= nx && nx < N &&
+                   0 <= ny && ny < M && box[nx][ny] == 0)
                 {
-                    visited[nextRow][nextCol] = true;
-                    box[nextRow][nextCol] = 1; 
-                    q.add(new int[]{nextRow, nextCol, count + 1});
-                     if(!q.isEmpty()){
-                         answer = count;
-                     } 
-
+                    box[nx][ny] = box[x][y] + 1;
+                    q.offer(new int[]{nx, ny});
                 }
             }
         }
-
-        for(int i = 0; i < N; i++){
-            for(int j = 0 ; j < M; j++){
-                // System.out.print(box[i][j] + " ");
-                if(box[i][j] == 0){
-                    answer = -1;
-                }
-            }
-            // System.out.println();
-        }
-
-        
-        bw.write(String.valueOf(answer));
-        bw.flush();
-        bw.close();
-        br.close();
-
     }
 }
+
+
+
+
